805fceed2851aa85adb0e68fe5985f7c4f8266ce
bhnprksh222 <bhnprksh222@gmail.com>
2020-04-30 01:03:51 +0530

    built recipe view!

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -1,15 +1,36 @@
 import { elements } from './base';
-import { Fraction } from 'fractional';
+// import { fracty } from 'fractional';
+import fracty from 'fracty';
 
 export const clearRecipe = () => {
     elements.recipe.innerHTML = '';
 };
+
+const formatCount = count => {
+    if(count) {
+        // count = 2.5 --->   5/2  --->  2 1/2
+        // count = 0.5 --->  1/2
+        const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
+
+        if(!dec) return count;
+
+        if(int === 0) {
+            const fr = new fracty(count);
+            return `${fr.numerator}/${fr.denominator}`;
+        } else {
+            const fr = new fracty(count - int);
+            return `${int} ${fr.numerator}/${fr.denominator}`;
+        }
+    }
+      return '?';
+};
+
 const createIngredient = ingredient => `
     <li class="recipe__item">
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
         </svg>
-    <div class="recipe__count">${ingredient.count}</div>
+    <div class="recipe__count">${fracty(ingredient.count)}</div>
     <div class="recipe__ingredient">
         <span class="recipe__unit">${ingredient.unit}</span>
         ${ingredient.ingredient}
